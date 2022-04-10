5805aebaf165a2e27351f1852c775357e472ae3b
ludmila <ludmila.romanovskaya@gmail.com>
2022-04-10 16:41:33 +0300

    create spring boot application

---
+++ src/main/java/com/familycode/myfamilyspringboot/MyFamilySpringBootApplication.java
@@ -0,0 +1,18 @@
+package com.familycode.myfamilyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class MyFamilySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFamilySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/familycode/myfamilyspringboot/MyFamilySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familycode.myfamilyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFamilySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
