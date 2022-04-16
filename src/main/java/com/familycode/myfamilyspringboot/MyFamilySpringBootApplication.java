48a01d7157bfb9fe3313f3ec3bd7098a8f29df96
ludmila <ludmila.romanovskaya@gmail.com>
2022-04-16 19:09:46 +0300

    add custom css theme to vaadin views

--- src/main/java/com/familycode/myfamilyspringboot/MyFamilySpringBootApplication.java
+++ src/main/java/com/familycode/myfamilyspringboot/MyFamilySpringBootApplication.java
@@ -1,13 +1,17 @@
 package com.familycode.myfamilyspringboot;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyFamilySpringBootApplication {
+@Theme(value = "customtheme")
+public class MyFamilySpringBootApplication extends SpringBootServletInitializer implements AppShellConfigurator {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyFamilySpringBootApplication.class, args);
+		SpringApplication.run(com.familycode.myfamilyspringboot.MyFamilySpringBootApplication.class, args);
 	}
 
 }
